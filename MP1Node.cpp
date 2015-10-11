e4b83e7565bc3fb5f297bde6223a9945ccb9e8c9
nguyenhuunghia <n9uy3nziz@gmail.com>
2015-10-11 07:09:36 -0700

    recvCallback

--- MP1Node.cpp
+++ MP1Node.cpp
@@ -218,6 +218,34 @@ bool MP1Node::recvCallBack(void *env, char *data, int size ) {
 	/*
 	 * Your code goes here
 	 */
+		assert(size >= sizeof(MessageHdr));
+		MessageHdr* msg = (MessageHdr*) data;
+		Address *src_addr = (Address*)(msg+1);
+		size -= sizeof(MessageHdr) + sizeof(Address) +1;
+		data += sizeof(MessageHdr) + sizeof(Address) +1;
+
+		switch (msg->msgType){
+			case JOINREQ:
+				onJoin(src_addr, data, size);
+				onHeartbeat(src_addr, data, size);
+				break;
+			case PING:
+				onHeartbeat(src_addr, data, size);
+				break;
+			case JOINREP:
+				{
+				memberNode->inGroup = 1;
+				stringstream msg;
+				msg << "JOINREP from " << src_addr->getAddress();
+				msg << "data" << *(long*)(data);
+				log->LOG(&memberNode->addr, msg.str().c_str());
+				onHeartbeat(src_addr, data, size);
+				break;
+				}
+			default:
+				log->LOG(&memberNode->addr, "Received other message");
+				break;
+		}
 }
 
 /**
